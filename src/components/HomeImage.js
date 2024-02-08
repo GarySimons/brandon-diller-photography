071681dc34609e2e0cec13567451375831a7801f
kvenner <gary.simons@miomni.com>
2024-02-08 17:26:23 +0000

    Home page done with background image

---
+++ src/components/HomeImage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HomeImageStyles.css'
+
+const HomeImage = () => {
+  return (
+    <div className='background-image'>
+        <h1 className='intials'>BD</h1>
+        <h2 className='subhead'>ARCHITECTURAL PHOTOGAPHER</h2>
+    </div>
+  )
+}
+
+export default HomeImage
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HomeImage from '../components/HomeImage'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
+        <HomeImage  />
         <Footer />
     </div>
   )
