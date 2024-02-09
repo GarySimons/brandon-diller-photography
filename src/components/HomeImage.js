ccb9731b7462aeb0f16430b4f27079b3e4488f42
kvenner <gary.simons@miomni.com>
2024-02-09 06:06:23 +0000

    Home page - text made mobile responsive

--- src/components/HomeImage.js
+++ src/components/HomeImage.js
@@ -4,8 +4,11 @@ import './HomeImageStyles.css'
 const HomeImage = () => {
   return (
     <div className='background-image'>
-        <h1 className='intials'>BD</h1>
-        <h2 className='subhead'>ARCHITECTURAL PHOTOGAPHER</h2>
+        <div className='content-container'>
+          <h1 className='intials'>BD</h1>
+          <h2 className='subhead'>ARCHITECTURAL PHOTOGAPHY</h2>
+          <p className='name'>Brandon Diller</p>
+        </div>
     </div>
   )
 }
