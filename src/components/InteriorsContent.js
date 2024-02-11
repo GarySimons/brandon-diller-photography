e67170d722fa89ecd63422bd51b0a34fc41227cb
kvenner <gary.simons@miomni.com>
2024-02-11 19:22:06 +0000

    Renamed interior images/rows

--- src/components/InteriorsContent.js
+++ src/components/InteriorsContent.js
@@ -8,31 +8,30 @@ export const InteriorsContent = () => {
       <div className="interiors-page-header">Interiors</div>
 
       <div class="interiors-image-grid">
-        <div className="int-row-two">
-          <div class="int-image-five"></div>
-          <div class="int-row-two-stack">
-            <div class="int-image-three"></div>
-            <div class="int-image-four"></div>
-          </div>
-        </div>
-        <div className="int-row-three">
-          <div class="int-image-six"></div>
-        </div>
         <div className="int-row-one">
           <div class="int-image-one"></div>
-          <div class="int-image-two"></div>
+          <div class="int-row-one-stack">
+            <div class="int-image-two"></div>
+            <div class="int-image-three"></div>
+          </div>
         </div>
-
-        <div className="int-row-five">
-          <div class="int-row-five-stack">
-            <div class="int-image-ten"></div>
-            <div class="int-image-eleven"></div>
+        <div className="int-row-two">
+          <div class="int-image-four"></div>
+        </div>
+        <div className="int-row-three">
+          <div class="int-image-five"></div>
+          <div class="int-image-six"></div>
+        </div>
+        <div className="int-row-four">
+          <div class="int-row-four-stack">
+            <div class="int-image-seven"></div>
+            <div class="int-image-eight"></div>
           </div>
           <div class="int-image-nine"></div>
         </div>
-        <div className="int-row-four">
-          <div class="int-image-seven"></div>
-          <div class="int-image-eight"></div>
+        <div className="int-row-five">
+          <div class="int-image-ten"></div>
+          <div class="int-image-eleven"></div>
         </div>
       </div>
     </div>
