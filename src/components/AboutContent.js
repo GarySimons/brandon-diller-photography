c701be3fc811f3e03303cf5fb0fa8edf7c2b88cc
kvenner <gary.simons@miomni.com>
2024-02-13 08:30:41 +0000

    Mobile nav - padding to move text up

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -14,21 +14,21 @@ const AboutContent = () => {
               Brandon is an English-German photographer. He is best known for
               his large format, highly detailed architectural photography,
               employing his unique style and view of the world. He collaborates
-              with leading architects, working on world wide projects of all
+              with leading architects, working on world-wide projects of all
               scales, both commercial and residential.
             </p>
             <p className="about-text">
-              Born in Cambridge UK to an English mother and German
-              father, his childhood was spent living between Cambridge and
-              Berlin. He is bilingual, speaking English and German. His love of
-              photography started in Berlin at the age of ten, when his
-              photographer Grandmother gave him his first camera.
+              Born in Cambridge UK to an English mother and German father, his
+              childhood was spent living between Cambridge and Berlin. He is
+              bilingual, speaking English and German. His love of photography
+              started in Berlin at the age of ten, when his photographer
+              Grandmother gave him his first camera.
             </p>
             <p className="about-text">
               He studied photography at Berlin University of the Arts, where he
-              earned a Bachelor of Arts (BA). Brandon then traveled extensively
-              in the US and Canada working with many influential photographers,
-              and developing his signature style.
+              earned a Bachelor of Arts &#40;BA&#41;. Brandon then travelled
+              extensively in the US and Canada working with many influential
+              photographers and developing his signature style.
             </p>
             <p className="about-text">
               After time spent based in New York building his client base,
